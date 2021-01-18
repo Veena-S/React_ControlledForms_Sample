d772dd553311ff0c38ea33ce8fa7de72e4e5c67a
Veena-S <veenasivan@gmail.com>
2021-01-18 08:31:27 +0800

    pre-class

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function Form() {
+  const [name, setName] = useState("");
+
+  const handleNameChange = (event) => {
+    const inputName = event.target.value;
+
+    console.log(inputName);
+
+    if (inputName.match(/^[a-zA-Z]+$/)) {
+      setName(inputName);
+    }
+  };
+
+  return (
+    <div>
+      <input value={name} onChange={handleNameChange} />
+      <div>name: {name}</div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      This is App.jsx
+      <Form/>
     </div>
   );
 }
